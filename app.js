753e04aaf3fa44df4d51a3945c9ae2e86949f11f
mrpeepernumber007 <federico.e.naso@gmail.com>
2023-10-19 15:59:02 -0300

    Add book functionality

---
+++ app.js
@@ -0,0 +1,82 @@
+const library = document.querySelector('.library')
+const wrapper = document.getElementById('wrapper')
+const formContainer = document.querySelector('.form-container')
+const libContainer = document.querySelector('.library-container')
+const newBookBtn = document.querySelector('.new-book-btn')
+
+formContainer.classList.add('invisible')
+const odinLibrary = []
+
+newBookBtn.addEventListener('click', toggleForm)
+
+function toggleForm() {
+    wrapper.classList.toggle('active-form')
+    libContainer.classList.toggle('library-area')
+    formContainer.classList.toggle('form-area')
+    formContainer.classList.toggle('invisible')
+}
+
+function intoDom(bookObj) {
+    const bookCard = document.createElement('div')
+    bookCard.classList.add('card')
+
+    const title = document.createElement('h2')
+    title.classList.add('book-title')
+
+    const author = document.createElement('p')
+    author.classList.add('book-author')
+    
+    const genre = document.createElement('p')
+    genre.classList.add('book-genre')
+    
+    const pages = document.createElement('p')
+    pages.classList.add('book-pages')
+    
+    const read = document.createElement('p')
+    read.classList.add('book-read')
+    
+    const delBtn = document.createElement('div')
+    delBtn.classList.add('del-btn')
+
+    title.textContent = bookObj.title
+    author.textContent = bookObj.author
+    genre.textContent = bookObj.genre
+    pages.textContent = bookObj.pages
+    read.textContent = 'read'
+    delBtn.textContent = 'X'
+
+    bookCard.appendChild(title)
+    bookCard.appendChild(author)
+    bookCard.appendChild(genre)
+    bookCard.appendChild(pages)
+    bookCard.appendChild(read)
+    bookCard.appendChild(delBtn)
+
+
+    // odinLibrary.push(bookOne)
+    library.appendChild(bookCard)
+}
+
+//test with library
+const bookOne = {
+    title: 'Nuestra parte de noche',
+    author: 'Mariana Enriquez',
+    genre: 'Terror',
+    pages: 666,
+}
+const bookTwo = {
+    title: 'Pepe grillo',
+    author: 'Pepina Grilla',
+    genre: 'Drama',
+    pages: '300',
+}
+
+const addBone = document.getElementById('add-bone')
+odinLibrary.push(bookOne)
+odinLibrary.push(bookTwo)
+
+addBone.addEventListener('click', iterateLib)
+
+function iterateLib () {
+    odinLibrary.forEach((book) => intoDom(book))
+}
